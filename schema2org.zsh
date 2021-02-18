169b30ea113f6015d04ab0dbcefebc8fe9a59a1d
Micah Elliott <mde@micahelliott.com>
2021-02-17 17:24:58 -0800

    :bug: Don’t hard-code db name in schema2org

--- schema2org.zsh
+++ schema2org.zsh
@@ -1,7 +1,9 @@
-#! /bin/bash
+#! /bin/zsh
 
 # Convert a schema dump from SQL DDL to a seed of ORG for filling out docs
 
-pg_dump -s cc |
+db=${DBDOC_DB?provide DB name as seed}
+
+pg_dump -s $db |
     sed -e '/^CREATE TABLE public\.[a-z_]* (/,/^)\;/!d' -e 's/CREATE TABLE public\.//' |
     gsed -r -e 's/^    ([a-z_]+) .*/- \1 ::/' -e 's/ \($//' -e 's/\);//' -e 's/^([a-z]+)/* \1/'
