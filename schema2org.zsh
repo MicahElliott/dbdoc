c9426593871452fd9f0bb0c83ad9b48b39204de9
Micah Elliott <mde@micahelliott.com>
2021-08-10 12:56:27 -0700

    :sparkles: Generate help text as part of seed doc

--- schema2org.zsh
+++ schema2org.zsh
@@ -4,6 +4,22 @@
 
 db=${DBDOC_DB?provide DB name as seed}
 
+print "#+Title: $DBDOC_DB\n"
+
+print \
+"This is the dbdoc description file for the database. See the
+[[https://github.com/micahelliott/dbdoc][dbdoc README]]
+for more detailed instructions on its purpose and expanding it. This file
+contains short documentation for any tables and columns that could use
+even the slightest bit of explanation.
+
+Edit this file whenever you make schema changes. And be a good citizen
+by helping to grow this file any time you're touching a table!
+The remainder of this file will be used processed into comment
+descriptions that will be visible in your SQL client, and can also be
+exported as HTML.
+"
+
 pg_dump -s $db |
     sed -e '/^CREATE TABLE public\.[a-z_]* (/,/^)\;/!d' -e 's/CREATE TABLE public\.//' |
     gsed -r -e 's/^    ([a-z_]+) .*/- \1 ::/' -e 's/ \($//' -e 's/\);//' -e 's/^([a-z]+)/* \1/'
